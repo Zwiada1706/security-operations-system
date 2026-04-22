d03ef13ff3a7a2b429eeb369dc115faf7bf0b01b
Zwiada1706 <makwarelazwiada2003@gmail.com>
2026-04-22 21:07:32 +0200

    configuring the beginning of the project

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold">
+        Security Operations System
+      </h1>
+    </main>
+  );
+}
